ebe3959aa3cc032a456721f5afc65c34b34c3c41
Matt Wilkin and Noah Yasskin <matt@>
2016-09-06 11:07:57 -0700

    add View for animals and an animal controller action

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,6 +1,12 @@
-class AnimalController < ApplicationController
+class AnimalsController < ApplicationController
   def index
     @animals = Animal.all
     render :index
   end
+
+  def show
+    @list = List.find(params[:id])
+    render :show
+  end
+
 end
