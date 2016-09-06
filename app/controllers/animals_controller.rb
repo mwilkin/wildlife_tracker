a30c15ae04aa9dc88f38ddfd676679ae851d6891
Matt Wilkin and Noah Yasskin <noah@>
2016-09-06 11:19:51 -0700

    create a new animal.

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -9,4 +9,23 @@ class AnimalsController < ApplicationController
     render :show
   end
 
+  def new
+    @animal = Animal.new
+    render :new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      redirect_to animals_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def animal_params
+    params.require(:animal).permit(:name)
+  end
+
 end
